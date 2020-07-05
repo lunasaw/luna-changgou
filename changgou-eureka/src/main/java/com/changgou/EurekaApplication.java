c7885acd780708e02546434c5a574e98a00fede9
luna <iszychen@gmail.com>
2020-07-05 15:51:39 +0800

    project init

---
+++ changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+
+/*****
+ * @Author: luna
+ * @Description: com.changgou
+ * 注册中心
+ ****/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+
+}
