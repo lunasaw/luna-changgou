18ac250b266aa579d4596c0f4fcdab11269d4a50
luna <iszychen@gmail.com>
2020-07-06 16:43:05 +0800

    album相册

---
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/AlbumMapper.java
@@ -0,0 +1,15 @@
+package com.changgou.goods.dao;
+
+import com.changgou.goods.pojo.Album;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Package: com.changgou.goods.dao
+ * @ClassName: AlbumMapper
+ * @Author: luna
+ * @CreateTime: 2020/7/6 15:39
+ * @Description:
+ */
+public interface AlbumMapper extends Mapper<Album> {
+
+}
---
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/AlbumService.java
@@ -0,0 +1,73 @@
+package com.changgou.goods.service;
+
+import com.changgou.goods.pojo.Album;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+ * @Package: com.changgou.goods.service
+ * @ClassName: AlbumService
+ * @Author: luna
+ * @CreateTime: 2020/7/6 15:40
+ * @Description:
+ */
+public interface AlbumService {
+
+    /***
+     * Album多条件分页查询
+     * @param album
+     * @param page
+     * @param size
+     * @return
+     */
+    PageInfo<Album> findPage(Album album, int page, int size);
+
+    /***
+     * Album分页查询
+     * @param page
+     * @param size
+     * @return
+     */
+    PageInfo<Album> findPage(int page, int size);
+
+    /***
+     * Album多条件搜索方法
+     * @param album
+     * @return
+     */
+    List<Album> findList(Album album);
+
+    /***
+     * 删除Album
+     * @param id
+     */
+    void delete(Long id);
+
+    /***
+     * 修改Album数据
+     * @param album
+     */
+    void update(Album album);
+
+    /***
+     * 新增Album
+     * @param album
+     */
+    void add(Album album);
+
+    /**
+     * 根据ID查询Album
+     *
+     * @param id
+     * @return
+     */
+    Album findById(Long id);
+
+    /***
+     * 查询所有Album
+     * @return
+     */
+    List<Album> findAll();
+}
+
